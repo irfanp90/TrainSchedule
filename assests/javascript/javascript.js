178625587be94056941b7a4f7cf1fcbefa3e3b45
irfanp90 <irfanp90@gmail.com>
2019-03-14 21:46:11 -0500

    html and java

---
+++ assests/javascript/javascript.js
@@ -0,0 +1,20 @@
+//initialize firebase
+var config = {
+  apiKey: "AIzaSyAMzbLJSFoKtbItO-VD4Bk-IpXqogECnes",
+  authDomain: "trainschedule-69f4d.firebaseapp.com",
+  databaseURL: "https://trainschedule-69f4d.firebaseio.com",
+  projectId: "trainschedule-69f4d",
+  storageBucket: "trainschedule-69f4d.appspot.com",
+  messagingSenderId: "696330048214"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//button to add train information
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  //get user input
+  var 
+});
